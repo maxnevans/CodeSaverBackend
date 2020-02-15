5cd168e58de7ec06a95099aae88844249fbfd66f
Maxim Shcherbakov <maxnevans@gmail.com>
2020-02-15 16:58:46 +0300

    routers bugs has been fixed

--- src/router/post-router.js
+++ src/router/post-router.js
@@ -11,15 +11,19 @@ class PostRouter {
     }
 
     _setupRoutes(router) {
-        router.post('*', this._upload.single());
-        
         router.post('/code/upload', this._upload.single('code-file'), this._uploadCodeSampleHandler.bind(this));
-        router.post('/code/create', this._createCodeSampleHandler.bind(this));
+        router.post('/code/create', this._upload.single(), this._createCodeSampleHandler.bind(this));
     }
 
-    _uploadCodeSampleHandler(req, res, next) {
+    _uploadCodeSampleHandler(req, res, next) {        
+        if (req.body['code-name'].length == 0)
+            return res.status(406).json({reason: 'code name should not be empty'});
+
+        if (req.file.buffer.length == 0)
+            return res.status(406).json({reason: 'code sample should not be empty'});
+
         this._connection.query('insert into code_samples set name = ?, author_id = ?, code = ?', 
-            [req.body['code-name'], 1, req.file.buffer.toString()], (error, results, fields) => {
+            [req.body['code-name'], 1, req.file.buffer.toString('utf-8')], (error, results, fields) => {
             if (error)
                 return next(error);
 
@@ -30,6 +34,12 @@ class PostRouter {
     }
 
     _createCodeSampleHandler(req, res, next) {
+        if (req.body['code-name'].length == 0)
+            return res.status(406).json({reason: 'code name should not be empty'});
+
+        if (req.body['code-sample'].length == 0)
+            return res.status(406).json({reason: 'code sample should not be empty'});
+
         this._connection.query('insert into code_samples set name = ?, author_id = ?, code = ?', 
             [req.body['code-name'], 1, req.body['code-sample']], (error, results, fields) => {
             if (error)
--- src/router/put-router.js
+++ src/router/put-router.js
@@ -11,29 +11,58 @@ class PutRouter {
     }
 
     _setupRoutes(router) {
-        router.put('*', this._upload.single());
         
-        router.put('/code/:sampleId', this._editCodeSampleHandler.bind(this));
+        router.put('/code/:sampleId', this._upload.single(), this._editCodeSampleHandler.bind(this));
         router.put('/code/upload/:sampleId', this._upload.single('code-file'), this._uploadCodeSampleHandler.bind(this));
     }
 
     _editCodeSampleHandler(req, res, next) {
+        if (req.body['code-name'].length == 0)
+            return res.status(406).json({reason: 'code name should not be empty'});
+
+        if (req.body['code-sample'].length == 0)
+            return res.status(406).json({reason: 'code sample should not be empty'});
+
         const editedTime = new Date();
         this._connection.query('update code_samples set name = ?, code = ?, edited_time = ? where id = ?', 
             [req.body['code-name'], req.body['code-sample'], editedTime, req.params.sampleId], (error, results, fields) => {
+                
             if (error)
                 return next(error);
+
+            if (results.affectedRows === 0) {
+                return res.status(404).end();
+            }
+
+            if (results.affectedRows > 1) {
+                return res.status(500).end();
+            }
 
             res.status(200).end();
         });
     }
 
     _uploadCodeSampleHandler(req, res, next) {
+        if (req.body['code-name'].length == 0)
+            return res.status(406).json({reason: 'code name should not be empty'});
+
+        if (req.file.buffer.length == 0)
+            return res.status(406).json({reason: 'code sample should not be empty'});
+
         const editedTime = new Date();
         this._connection.query('update code_samples set name = ?, code = ?, edited_time = ? where id = ?', 
-            [req.body['code-name'], req.file.buffer.toString(), req.params.sampleId, editedTime], (error, results, fields) => {
+            [req.body['code-name'], req.file.buffer.toString(), editedTime, req.params.sampleId], (error, results, fields) => {
+
             if (error)
                 return next(error);
+
+            if (results.affectedRows === 0) {
+                return res.status(404).end();
+            }
+
+            if (results.affectedRows > 1) {
+                return res.status(500).end();
+            }
 
             res.status(200).end();
         });
